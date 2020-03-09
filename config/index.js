69ed0afb96dbb0632e476aec8e170ef1bc602ef9
Fin Mavis <fin.mavis@yahoo.com>
2020-03-09 23:54:16 +0700

    Update og, add offline mode, and add manifest

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ module.exports = {
   description:
     'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
   author: 'Fin Mavis',
-  ogImage: '/images/opengraph.jpg',
+  ogImage: '/images/opengraph.png',
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -100,18 +100,18 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Fin Mavis',
+        short_name: 'Fin Mavis',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        background_color: '#216d8e',
+        theme_color: '#216d8e',
+        display: 'standalone',
+        icon: 'static/android-chrome-512x512.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     'gatsby-plugin-sitemap',
   ],
 };
